a88c42785b8a6efbf11a35899e3d73fcd02ae0d0
Carl Fredrik Ahl <carlfredrikahl@gmail.com>
2023-12-18 09:43:19 +0100

    Downloading more data about the phone (screen size, resolution, screen type and phone width)

--- app/src/main/java/com/example/webscraping/MainActivity.java
+++ app/src/main/java/com/example/webscraping/MainActivity.java
@@ -57,11 +57,24 @@ public class MainActivity extends AppCompatActivity {
 
                 for(int i = 0; i < 5; i++) {
                     String name = phonesData.get(i).text();
-                    String imgUrl = phonesData.select(".module-phones-link").select("img").eq(i).attr("src");
-                    Log.i("PHONE_TAG", "Name: " + name + ", ImgURL: " + imgUrl);
+                    String imgUrl = phonesData.select("img").eq(i).attr("src");
+                    String phoneUrl = phonesData.eq(i).attr("href");
+                    String completePhoneUrl = url + phoneUrl;
+
+                    //Visit each individual phone link to extract data about them
+                    Document phoneSpecsDoc = Jsoup.connect(completePhoneUrl).get();
+                    String screenSize = phoneSpecsDoc.select("td:contains(Size)").next().text();
+                    String screenResolution = phoneSpecsDoc.select("td:contains(Resolution)").next().text();
+                    String screenType = phoneSpecsDoc.select("td:contains(Type)").next().text();
+                    String phoneDimensions = phoneSpecsDoc.select("td:contains(Dimensions)").next().text();
+                    //String[] test = phoneDimensions.split(" x ");
+                    String phoneWidth = phoneDimensions.split(" x ")[1];
+
+                    Log.i("PHONE_TAG", "Name: " + name + ", screen size: " + screenSize + "screen resolution: " + screenResolution
+                    + "screen type" + screenType + "phone width: " + phoneWidth);
 
                     //Add the phones to phone models
-                    phoneModels.add(new PhoneModel(name, imgUrl));
+                    phoneModels.add(new PhoneModel(name, imgUrl, "test", "test", "test", "test"));
                     publishProgress();
                 }
             } catch (IOException ioe) {
--- app/src/main/java/com/example/webscraping/PhoneModel.java
+++ app/src/main/java/com/example/webscraping/PhoneModel.java
@@ -3,12 +3,20 @@ package com.example.webscraping;
 public class PhoneModel {
     private String name;
     private String imageUrl;
+    private String screenSize;
+    private String resolution;
+    private String displayType;
+    private String phoneWidth;
 
     public PhoneModel() { }
 
-    public PhoneModel(String name, String imageUrl) {
+    public PhoneModel(String name, String imageUrl, String screenSize, String resolution, String displayType, String phoneWidth) {
         this.name = name;
         this.imageUrl = imageUrl;
+        this.screenSize = screenSize;
+        this.resolution = resolution;
+        this.displayType = displayType;
+        this.phoneWidth = phoneWidth;
     }
 
     public String getName() {
@@ -17,6 +25,22 @@ public class PhoneModel {
 
     public String getImageUrl() {
         return imageUrl;
+    }
+
+    public String getScreenSize() {
+        return screenSize;
+    }
+
+    public String getDisplayType() {
+        return displayType;
+    }
+
+    public String getResolution() {
+        return resolution;
+    }
+
+    public String getPhoneWidth() {
+        return phoneWidth;
     }
 }
 
